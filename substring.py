ccce00c8868c56acf2c91c2d30eb685f75e754e8
raghava1998 <41614794+raghava1998@users.noreply.github.com>
2019-07-10 13:21:22 +0530

    Create substring.py

---
+++ substring.py
@@ -0,0 +1,19 @@
+s=input()
+q=['a','b,'c','d','e','f','g','h,'i','j','k','l','m','n','o','p','q','s','t','u','v','w','x','y','z']
+c=0
+for i in s:
+    for j in l:
+      {
+       if i==j:
+       c=c+1
+       q.remove(j)
+print(c)
+       
+       
+     
+      
+      
+       
+
+
+
